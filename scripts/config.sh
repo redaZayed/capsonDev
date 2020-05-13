9962caf36122d7821d907e3e6a579195da540477
redaZayed <63560963+redaZayed@users.noreply.github.com>
2020-05-13 03:02:28 +0200

    Add files via upload

---
+++ scripts/config.sh
@@ -0,0 +1,12 @@
+
+export user="redazayed"
+
+# Docker image parameters
+export appname=udacity-capstone
+export version=0.1
+
+export hostport=6000
+export containerport=5000
+
+# Create dockerpath
+export dockerpath=$user/$appname
